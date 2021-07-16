be4de8f4d37825f68234d62a9b79908ca69c8d3a
tenglovemeng <1334500819@qq.com>
2021-07-16 16:52:44 +0800

    代码重构

--- src/com/tengfei/f9framework/file/F9StandardWebappFile.java
+++ src/com/tengfei/f9framework/file/F9StandardWebappFile.java
@@ -1,24 +1,11 @@
 package com.tengfei.f9framework.file;
 
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.tengfei.f9framework.notification.F9Notifier;
-import com.tengfei.f9framework.setting.F9CustomizeModule;
-import com.tengfei.f9framework.setting.F9ProjectSetting;
 import com.tengfei.f9framework.setting.F9StandardModule;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
 /**
  * @author ztf
