e3d4c8eb5e3382ade0591501bd29295e8a00aff5
tenglovemeng <1334500819@qq.com>
2021-08-18 23:22:03 +0800

    增加datagrid提醒

--- src/com/tengfei/f9framework/postcompletetemplate/F9DataGridTemplate.java
+++ src/com/tengfei/f9framework/postcompletetemplate/F9DataGridTemplate.java
@@ -1,28 +1,42 @@
 package com.tengfei.f9framework.postcompletetemplate;
 
+import com.intellij.codeInsight.template.Template;
+import com.intellij.codeInsight.template.TemplateManager;
+import com.intellij.codeInsight.template.postfix.templates.PostfixTemplate;
 import com.intellij.codeInsight.template.postfix.templates.PostfixTemplateWithExpressionSelector;
 import com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplatesUtils;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiIdentifier;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.tengfei.f9framework.util.EditorManage;
 import org.jetbrains.annotations.NotNull;
 
-public class F9DataGridTemplate extends PostfixTemplateWithExpressionSelector {
-    protected F9DataGridTemplate() {
-        super("datagrid", "datagrid", "getDataGridModel", JavaPostfixTemplatesUtils.selectorTopmost(element -> {
-           if(!(element instanceof PsiIdentifier) && element.getParent() != null && element.getParent().getParent() != null){
-               return false;
-           }
-           return element.getParent().getParent() instanceof PsiClass;
+import java.util.List;
 
-        }));
+public class F9DataGridTemplate extends PostfixTemplate {
+
+
+    protected F9DataGridTemplate() {
+        super("datagrid", "getDataGridModel");
     }
 
     @Override
-    protected void expandForChooseExpression(@NotNull PsiElement expression, @NotNull Editor editor) {
-        EditorManage.getInstance(editor).removeExpression(expression);
+    public boolean isApplicable(@NotNull PsiElement context, @NotNull Document copyDocument, int newOffset) {
+        return context instanceof PsiIdentifier;
+    }
 
+    @Override
+    public void expand(@NotNull PsiElement context, @NotNull Editor editor) {
+        EditorManage.getInstance(editor).removeExpression(context);
+        Project project =editor.getProject();
+
+        assert project != null;
+        final TemplateManager manager = TemplateManager.getInstance(project);
+        final String stringTemplate = "System.out.println(\"kdkdk\");";
+        final Template template = manager.createTemplate("", "", stringTemplate);
+        template.setToReformat(true);
+        manager.startTemplate(editor, template);
     }
 }
