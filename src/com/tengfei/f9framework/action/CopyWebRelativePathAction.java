0302c8f7c4c617e0328c6fb8cb99cfb8393af1ef
tenglovemeng <1334500819@qq.com>
2021-08-22 19:43:10 +0800

    增加复制相对路径功能

---
+++ src/com/tengfei/f9framework/action/CopyWebRelativePathAction.java
@@ -0,0 +1,36 @@
+package com.tengfei.f9framework.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.ide.CopyPasteManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.tengfei.f9framework.file.F9FileFactory;
+import com.tengfei.f9framework.file.F9WebappFile;
+import com.tengfei.f9framework.notification.F9Notifier;
+
+import java.awt.datatransfer.StringSelection;
+
+public class CopyWebRelativePathAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
+        if(virtualFile == null) {
+            F9Notifier.notifyWarning(e.getProject(),"为获取到虚拟文件");
+            return;
+        }
+        F9WebappFile f9WebAppFile = null;
+        try {
+            f9WebAppFile = F9FileFactory.getInstance(e.getProject()).createF9WebAppFile(virtualFile, e.getProject());
+        } catch (Exception exception) {
+            F9Notifier.notifyError(e.getProject(),"不支持的文件类型");
+            return;
+        }
+        String webRelativePath = f9WebAppFile.getWebRelativePath();
+        CopyPasteManager.getInstance().setContents(new StringSelection(webRelativePath));
+        F9Notifier.notifyMessage(e.getProject(),"web相对路径已复制进剪切板");
+
+    }
+}
